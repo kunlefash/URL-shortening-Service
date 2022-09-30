e2b57c6dd8e7220a4a6595665f19c6251e7cfe08
kunlefash <kunlefashmayop@gmail.com>
2022-09-30 16:19:47 -0300

    Create POST route that creates the Short URL

--- backend/config/URL-Shortening-Service/routes/redirect.ts
+++ backend/config/URL-Shortening-Service/routes/redirect.ts
@@ -1,63 +1,36 @@
 const express = require('express')
-const validUrl = require('valid-url')
-const shortid = require('shortid')
 
 const router = express.Router()
 
 const Url = require('../models/Url')
 
-const baseUrl = 'http:localhost:5000'
+// : app.get(/:code)
 
-router.post('/shorten', async (req, res) => {
-    const {
-        longUrl
-    } = req.body // destructures the longUrl from req.body.longUrl
+// @route       GET /:code
+// @description    Redirect to the long/original URL 
+router.get('/:code', async (req, res) => {
+    try {
+        // find a document match to the code in req.params.code
+        const url = await Url.findOne({
+            urlCode: req.params.code
+        })
+        if (url) {
+            // when valid we perform a redirect
+            return res.redirect(url.longUrl)
+        } else {
+            // else return a not found 404 status
+            return res.status(404).json('No URL Found')
+        }
 
-    // check base url if valid using the validUrl.isUri method
-    if (!validUrl.isUri(baseUrl)) {
-        return res.status(401).json('Invalid base URL')
     }
-
-    // if valid, we create the url code
-    const urlCode = shortid.generate()
-
-    // check long url if valid using the validUrl.isUri method
-    if (validUrl.isUri(longUrl)) {
-        try {
-            /* The findOne() provides a match to only the subset of the documents 
-            in the collection that match the query. In this case, before creating the short URL,
-            we check if the long URL was in the DB ,else we create it.
-            */
-            let url = await Url.findOne({
-                longUrl
-            })
-
-            // url exist and return the respose
-            if (url) {
-                res.json(url)
-            } else {
-                // join the generated short code the the base url
-                const shortUrl = baseUrl + '/' + urlCode
-
-                // invoking the Url model and saving to the DB
-                url = new Url({
-                    longUrl,
-                    shortUrl,
-                    urlCode,
-                    date: new Date()
-                })
-                await url.save()
-                res.json(url)
-            }
-        }
-        // exception handler
-        catch (err) {
-            console.log(err)
-            res.status(500).json('Server Error')
-        }
-    } else {
-        res.status(401).json('Invalid longUrl')
+    // exception handler
+    catch (err) {
+        console.error(err)
+        res.status(500).json('Server Error')
     }
 })
 
+
 module.exports = router
+
+export {};
